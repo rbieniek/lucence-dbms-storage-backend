9f2ca474ff10f11686d540d547795f7a2a078b0a
Rainer Bieniek <Rainer.Bieniek@vodafone.com>
2017-03-13 01:04:10 +0100

    Fixe test jar builder

--- lucene-jdbc-backend-test/src/main/java/de/bieniekconsulting/lucene/jdbc/test/LuceneJdbcTestJarBuilder.java
+++ lucene-jdbc-backend-test/src/main/java/de/bieniekconsulting/lucene/jdbc/test/LuceneJdbcTestJarBuilder.java
@@ -3,10 +3,12 @@ package de.bieniekconsulting.lucene.jdbc.test;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 
-import de.bieniekconsulting.springframework.support.TestConfiguration;
+import de.bieniekconsulting.lucene.jdbc.directory.JdbcDirectory;
+import de.bieniekconsulting.lucene.jdbc.types.LogRecord;
 
 public class LuceneJdbcTestJarBuilder {
 	public static JavaArchive extensionJar() {
-		return ShrinkWrap.create(JavaArchive.class).addClass(TestConfiguration.class);
+		return ShrinkWrap.create(JavaArchive.class).addPackage(JdbcDirectory.class.getPackage())
+				.addPackage(LogRecord.class.getPackage());
 	}
 }
